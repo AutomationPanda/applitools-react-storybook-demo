235d1825e405802d0e18f32772d4e43bd2fee597
Pandy Knight (@AutomationPanda) <andylpk.247@gmail.com>
2021-10-30 22:04:09 -0400

    Added scroll buttons to main page

--- src/App.js
+++ src/App.js
@@ -2,33 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './stories/Button.jsx'
 import TextInput from './stories/TextInput.jsx'
+import ScrollButton from './stories/ScrollButton';
 
 function App() {
   return (
     <div className="App">
 
-      <header className="App-header">
+      <header className="App-header" id="slide1">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Let's learn how to visually test components using Applitools Eyes!</p>
+        <p>We will use this React app with Storyboard components.</p>
         <p>
-          Let's learn how to visually test components using Applitools Eyes!
+          <a
+            className="App-link"
+            href="https://github.com/AutomationPanda/applitools-react-storybook-demo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Demo Project Repository
+          </a>
         </p>
-        <p>
-          We will use this React app with Storyboard components.
-          Scroll down to learn.
-        </p>
-        <p>
-        <a
-          className="App-link"
-          href="https://github.com/AutomationPanda/applitools-react-storybook-demo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Demo Project Repository
-        </a>
-        </p>
+        <p className="scroll-arrow-margin"><ScrollButton scrollToId="slide2"/></p>
       </header>
 
-      <header className="App-header">
+      <header className="App-header" id="slide2">
         <p>Web apps are full of small, reusable pieces called <b>components</b>.</p>
 
         <div class="component-container">
@@ -36,24 +33,27 @@ function App() {
           <div><p><Button label="Click me!" primary="true" size="large"></Button></p></div>
           <div><p>Here's one named <code>TextInput</code>:</p></div>
           <div><p><TextInput/></p></div>
+          <div><p>The <code>ScrollButton</code> is a component, too:</p></div>
+          <div><p><ScrollButton/></p></div>
         </div>
 
         <p>
           Components enable developers to create uniform UIs without duplicating code.
         </p>
         <p>
-          This project uses <a
+          This React app uses <a
             className="App-link"
             href="https://storybook.js.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
           Storybook
-          </a> to build components.
+          </a> to build its components.
         </p>
+        <p className="scroll-arrow-margin"><ScrollButton scrollToId="slide3" /></p>
       </header>
 
-      <header className="App-header">
+      <header className="App-header" id="slide3">
         <p>What happens if a component changes? What if our button...</p>
         <p><Button label="Click me!" primary="true" size="large"></Button></p>
         <p>... becomes rectangular?</p>
@@ -61,15 +61,17 @@ function App() {
         <p>... or changes color?</p>
         <p><Button label="Click me!" primary="true" size="large" backgroundColor="red"></Button></p>
         <p>Is that acceptable? Is that bad? Will anyone notice?</p>
+        <p className="scroll-arrow-margin"><ScrollButton scrollToId="slide4" /></p>
       </header>
 
-      <header className="App-header">
+      <header className="App-header" id="slide4">
         <p>What if our button become enormous, like this?</p>
         <p><Button label="Click me!" primary="true" size="enormous"></Button></p>
         <p>Clearly, this could ruin the user experience on multiple pages.</p>
+        <p className="scroll-arrow-margin"><ScrollButton scrollToId="slide5" /></p>
       </header>
       
-      <header className="App-header">
+      <header className="App-header" id="slide5">
         <p>We need a way to visually check for component changes:</p>
         <div class="comparison-container">
           <div><p><Button label="Click me!" primary="true" size="large"></Button></p></div>
@@ -91,9 +93,10 @@ function App() {
         <p>
           It captures screenshots for each story and uses Visual AI for comparisons.
         </p>
+        <p className="scroll-arrow-margin"><ScrollButton scrollToId="slide6" /></p>
       </header>
 
-      <header className="App-header">
+      <header className="App-header" id="slide6">
         <p>Let's see how to visually test Storybook components with Applitools!</p>
         <div className="applitools-img-container">
           <img src="applitools-dashboard.png" className="applitools-img"/>
